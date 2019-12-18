8e93ecd87dbaab86de95c6d76be01588ab073192
MasatoMakino <unagiinu.g@gmail.com>
2019-12-18 20:31:16 +0900

    update : devDependencies.

---
+++ gulpfile.js/index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const { series, parallel } = require("gulp");
+
+const doc = require("gulptask-tsdoc").get();
+const server = require("gulptask-dev-server").get("./docs/demo");
+
+const { bundleDemo, watchDemo } = require("gulptask-demo-page").get({
+  body: `<canvas id="webgl-canvas" width="1280" height="768"></canvas>`
+});
+
+const { tsc, watchTsc } = require("gulptask-tsc").get();
+
+const watchTasks = async () => {
+  watchDemo();
+  watchTsc();
+};
+
+exports.start_dev = series(watchTasks, server);
+exports.build = series(tsc, parallel(bundleDemo, doc));
